b28e3cfad5b6db4ed09ab1cba7f0987cee52058e
Daniel Erazo <danielmauricioerazoespinoza@gmail.com>
2024-12-08 16:58:39 -0500

    Secret phone number added

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask, request, jsonify
 from modules.messages_service import send_summary_messages
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -10,6 +14,8 @@ def bot():
         to_number = request.json.get("to_number", "")
         if not to_number:
             return jsonify({"error": "to_number and message fields are required"}), 400
+        if to_number != os.environ.get("MY_PHONE_NUMBER"):
+            return jsonify({"error": "Sorry, this is not the correct phone number"}), 400
         return send_summary_messages(to_number=to_number)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
